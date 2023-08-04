fa4e14685e901be0c822017ded3d74a7db53dcd8
VithyaSangkavi <vithyasangkavi@gmail.com>
2023-08-04 11:39:30 +0530

    Defined Read, Update and Delete functionalities for exam model

--- backend/controllers/exam.controller.js
+++ backend/controllers/exam.controller.js
@@ -11,6 +11,76 @@ const createExam = async (req, res) => {
   }
 };
 
+const getAllExams = async (req, res) => {
+  try {
+    const exams = await Exam.findAll();
+    res.status(200).json(exams);
+  } catch (error) {
+    console.error('Error fetching exams:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const getExamById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const exam = await Exam.findByPk(id);
+    if (exam) {
+      res.status(200).json(exam);
+    } else {
+      res.status(404).json({ error: 'Exam not found' });
+    }
+  } catch (error) {
+    console.error('Error fetching exam:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const updateExamById = async (req, res) => {
+  const { id } = req.params;
+  const { examName, startDateAndTime, duration, examStatus, totalMarks, userID } = req.body;
+  try {
+    const exam = await Exam.findByPk(id);
+    if (exam) {
+      await exam.update({
+        examName,
+        startDateAndTime,
+        duration,
+        examStatus,
+        totalMarks,
+        userID,
+      });
+      res.status(200).json(exam);
+    } else {
+      res.status(404).json({ error: 'Exam not found' });
+    }
+  } catch (error) {
+    console.error('Error updating exam:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const deleteExamById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedExam = await Exam.destroy({
+      where: { examID: id },
+    });
+    if (deletedExam === 1) {
+      res.status(200).json({ message: 'Exam deleted successfully' });
+    } else {
+      res.status(404).json({ error: 'Exam not found' });
+    }
+  } catch (error) {
+    console.error('Error deleting exam:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
 module.exports = {
-  createExam
+  createExam,
+  getAllExams,
+  getExamById,
+  updateExamById,
+  deleteExamById,
 };
--- backend/routes/exam.routes.js
+++ backend/routes/exam.routes.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const examController = require('../controllers/exam.controller.js');
 
 router.post('/', examController.createExam);
+router.get('/', examController.getAllExams);
+router.get('/:id', examController.getExamById);
+router.put('/:id', examController.updateExamById);
+router.delete('/:id', examController.deleteExamById);
 
 module.exports = router;
