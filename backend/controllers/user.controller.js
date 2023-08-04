e66d342f0a9cb2652b7650bb3d1b3893910a9fb4
VithyaSangkavi <vithyasangkavi@gmail.com>
2023-08-04 10:54:43 +0530

    Defined Read, Update and Delete functionalities for user model

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -11,6 +11,69 @@ const createUser = async (req, res) => {
   }
 };
 
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Error fetching users:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByPk(id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    console.error('Error fetching user:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const updateUserById = async (req, res) => {
+  const { id } = req.params;
+  const { userName, userEmail, password, userType } = req.body;
+  try {
+    const user = await User.findByPk(id);
+    if (user) {
+      await user.update({ userName, userEmail, password, userType });
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    console.error('Error updating user:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const deleteUserById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedUser = await User.destroy({
+      where: { userID: id },
+    });
+    if (deletedUser === 1) {
+      res.status(200).json({ message: 'User deleted successfully' });
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    console.error('Error deleting user:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
 module.exports = {
-  createUser
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUserById,
+  deleteUserById
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/user.controller.js');
 
 router.post('/', userController.createUser);
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getUserById);
+router.put('/:id', userController.updateUserById);
+router.delete('/:id', userController.deleteUserById);
 
 module.exports = router;
