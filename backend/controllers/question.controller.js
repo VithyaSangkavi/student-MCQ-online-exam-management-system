09f3cbb3fb414eb98c646abff315efadf4da31f2
VithyaSangkavi <vithyasangkavi@gmail.com>
2023-08-04 12:11:07 +0530

    Backend for question table

---
+++ backend/controllers/question.controller.js
@@ -0,0 +1,79 @@
+const Question = require('../models/question');
+
+const createQuestion = async (req, res) => {
+  try {
+    const { questionID, questionNo, questionText, questionMarks, examID } = req.body;
+    const newQuestion = await Question.create({ questionID, questionNo, questionText, questionMarks, examID });
+    res.status(201).json(newQuestion);
+  } catch (error) {
+    console.error('Error creating question:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const getAllQuestions = async (req, res) => {
+  try {
+    const questions = await Question.findAll();
+    res.status(200).json(questions);
+  } catch (error) {
+    console.error('Error fetching questions:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const getQuestionById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const question = await Question.findByPk(id);
+    if (question) {
+      res.status(200).json(question);
+    } else {
+      res.status(404).json({ error: 'Question not found' });
+    }
+  } catch (error) {
+    console.error('Error fetching question:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const updateQuestionById = async (req, res) => {
+  const { id } = req.params;
+  const { questionNo, questionText, questionMarks, examID } = req.body;
+  try {
+    const question = await Question.findByPk(id);
+    if (question) {
+      await question.update({ questionNo, questionText, questionMarks, examID });
+      res.status(200).json(question);
+    } else {
+      res.status(404).json({ error: 'Question not found' });
+    }
+  } catch (error) {
+    console.error('Error updating question:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const deleteQuestionById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedQuestion = await Question.destroy({
+      where: { questionID: id },
+    });
+    if (deletedQuestion === 1) {
+      res.status(200).json({ message: 'Question deleted successfully' });
+    } else {
+      res.status(404).json({ error: 'Question not found' });
+    }
+  } catch (error) {
+    console.error('Error deleting question:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+module.exports = {
+  createQuestion,
+  getAllQuestions,
+  getQuestionById,
+  updateQuestionById,
+  deleteQuestionById,
+};
---
+++ backend/models/question.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../configs/database');
+const Exam = require('./exam');
+
+const Question = sequelize.define('question', {
+  questionID: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  questionNo: {
+    type: DataTypes.INTEGER,
+  },
+  questionText: {
+    type: DataTypes.TEXT,
+  },
+  questionMarks: {
+    type: DataTypes.FLOAT,
+  },
+  examID: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: Exam,
+      key: 'examID',
+    },
+  },
+}, {
+  tableName: 'question',
+  timestamps: false
+});
+
+// 1:N - exam-question
+Exam.hasMany(Question, {
+  foreignKey: 'examID', 
+});
+Question.belongsTo(Exam, {
+  foreignKey: 'examID', 
+});
+
+module.exports = Question;
