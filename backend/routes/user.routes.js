9a890b9dba682ff382f994c1068fa83ed0132043
VithyaSangkavi <vithyasangkavi@gmail.com>
2023-08-07 13:11:57 +0530

    Added authenticate middleware before controller

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,16 +3,12 @@ const router = express.Router();
 const userController = require('../controllers/user.controller.js');
 const authenticate = require('../middleware/authenticate.js')
 
-router.post('/', userController.createUser);
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
-router.put('/:id', userController.updateUserById);
-router.delete('/:id', userController.deleteUserById);
+router.post('/', authenticate, userController.createUser);
+router.get('/', authenticate, userController.getAllUsers);
+router.get('/:id', authenticate, userController.getUserById);
+router.put('/:id', authenticate, userController.updateUserById);
+router.delete('/:id', authenticate, userController.deleteUserById);
 
 router.post('/login', userController.loginUser)
-
-router.get('/protected', authenticate, (req, res) => {
-    res.json({ message: 'Protected route', user: req.user });
-  });
 
 module.exports = router;
