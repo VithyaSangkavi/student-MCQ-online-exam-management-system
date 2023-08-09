422ef5d54a82f4a2eb60be0c2e23de68f1988aac
VithyaSangkavi <vithyasangkavi@gmail.com>
2023-08-09 10:47:17 +0530

    Minor change

--- frontend/src/pages/loginForm.js
+++ frontend/src/pages/loginForm.js
@@ -31,23 +31,23 @@ function LoginForm() {
 
     return (
         <>
-            <div className='min-h-screen flex justify-center items-center'>
-                <div className='border-2 border[#1F2937] p-16'>
-                    <form onSubmit={handleSubmit}>
-                        <div>
-                            <label>Email Address</label> <br />
-                            <input type="email" placeholder="Email Address" className='border-2 border[#1F2937] w-[250px] h-[35px]' value={email} onChange={(e) => setEmail(e.target.value)} />
-                        </div>
-                        <br />
-                        <div>
-                            <label>Password</label> <br />
-                            <input type="password" placeholder="password" className='border-2 border[#1F2937] w-[250px] h-[35px]' value={password} onChange={(e) => setPassword(e.target.value)} />
-                        </div>
-                        <br />
-                        <button type="submit" className='bg-[#5850EC] pr-20 pl-20 pt-2 pb-2 text-white w-[250px] h-[35px] font-bold'>Sign in</button>
-                    </form>
-                </div>
-            </div>
+              <div className='min-h-screen flex justify-center items-center'>
+      <div className='border-2 border[#1F2937] p-16'>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email Address</label> <br />
+          <input type="email" placeholder="Email Address" className='border-2 border[#1F2937] w-[250px] h-[35px]' value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <br />
+        <div>
+          <label>Password</label> <br />
+          <input type="password" placeholder="password" className='border-2 border[#1F2937] w-[250px] h-[35px]' value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <br />
+        <button type="submit" className='bg-[#5850EC] pr-20 pl-20 pt-2 pb-2 text-white w-[250px] h-[35px] font-bold'>Sign in</button>
+      </form>
+      </div>
+    </div>
         </>
     );
 }
