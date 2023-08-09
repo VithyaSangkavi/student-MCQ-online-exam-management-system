30d1d76e98d3187fa0b8974ed93732b5507b1c6e
VithyaSangkavi <vithyasangkavi@gmail.com>
2023-08-09 09:20:50 +0530

    Login form interface

--- frontend/src/pages/loginForm.js
+++ frontend/src/pages/loginForm.js
@@ -2,48 +2,54 @@ import React, { useState } from 'react';
 import axios from 'axios'
 
 function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+      e.preventDefault();
 
-    try {
-      const response = await axios.post('http://localhost:3000/users/login', {
-        email,
-        password,
-      });
-  
-      if (response.status === 200) {
-        // Successful login
-        const data = response.data;
-        localStorage.setItem('token', data.token);
-        console.log('Login successful:', data);
-        // redirect
-      } else {
-        console.error('Login error:', response.data);
+      try {
+        const response = await axios.post('http://localhost:3000/users/login', {
+          userEmail: email,
+          password: password,
+        });
+
+        if (response.status === 200) {
+          // Successful login
+          const data = response.data;
+          localStorage.setItem('token', data.token);
+          console.log('Login successful:', data);
+          // redirect
+        } else {
+          console.error('Login error:', response.data);
+        }
+      } catch (error) {
+        console.error('Error:', error);
       }
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
+    };
 
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Email Address</label> <br/>
-        <input type="email" placeholder="Email Address" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <br />
-      <div>
-        <label>Password</label> <br/>
-        <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </div>
-      <br />
-      <button type="submit">Login</button>
-    </form>
-  );
+    return (
+        <>
+            <div className='min-h-screen flex justify-center items-center'>
+                <div className='border-2 border[#1F2937] p-16'>
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <label>Email Address</label> <br />
+                            <input type="email" placeholder="Email Address" className='border-2 border[#1F2937] w-[250px] h-[35px]' value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+                        <br />
+                        <div>
+                            <label>Password</label> <br />
+                            <input type="password" placeholder="password" className='border-2 border[#1F2937] w-[250px] h-[35px]' value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        <br />
+                        <button type="submit" className='bg-[#5850EC] pr-20 pl-20 pt-2 pb-2 text-white w-[250px] h-[35px] font-bold'>Sign in</button>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default LoginForm;
